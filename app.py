d1c7685079ec5db672d42b98a690418f50e1dafa
ngjamesng <just.james.ng@gmail.com>
2020-02-04 15:01:46 -0800

    add login/logout route. Add  register/authenticate class method for user. Add feedback class. Added html templates for login

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, session, flash
 from models import db, connect_db, User
-from forms import CreateUserForm
+from forms import CreateUserForm, LoginForm
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///flask-feedback'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///flask-feedback"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
 
 connect_db(app)
 db.create_all()
 
-app.config['SECRET_KEY'] = "secret"
+app.config["SECRET_KEY"] = "secret"
 
 
 @app.route("/")
@@ -19,29 +19,59 @@ def start():
 
 
 @app.route("/register", methods=["GET", "POST"])
-def register_user():
+def register():
     form = CreateUserForm()
 
     if form.validate_on_submit():
-        new_user = User(
+        new_user = User.register(
             username=form.username.data,
-            password=form.password.data,
+            pwd=form.password.data,
             email=form.email.data,
             first_name=form.first_name.data,
-            last_name=form.last_name.data)
+            last_name=form.last_name.data,
+        )
         db.session.add(new_user)
         db.session.commit()
         return redirect("/secret")
     else:
-        return render_template('register.html', form=form)
+        return render_template("register.html", form=form)
 
 
-# @app.route("/login", methods=["GET", "POST"])
-# def show_login():
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    form = LoginForm()
+    if form.validate_on_submit():
+
+        user = User.authenticate(username=form.username.data, pwd=form.password.data)
+
+        if user:
+            # session["username'] keeps user logged in
+            session["username"] = user.username
+            return redirect("/secret")
+
+        else:
+
+            form.username.errors = ["Please try your username and password again."]
+
+    return render_template("login.html", form=form)
 
 
+@app.route("/users/<username>", methods=["GET"])
+def show_user(username):
 
-@app.route("/secret", methods=["GET"])
-def show_secret():
+    if "username" not in session:
+        flash("you must be logged in to view!")
+        return redirect("/")
 
-    return "You made it!"
+    user = User.query.get_or_404(username)
+
+    return render_template("user.html", user=user)
+
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    if "username" in session:
+        session.pop("username")
+
+    return redirect("/")
--- models.py
+++ models.py
@@ -1,7 +1,9 @@
 """Models for Feedback"""
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 
 def connect_db(app):
@@ -14,11 +16,53 @@ def connect_db(app):
 class User(db.Model):
     """ User. username, password, email, first_name, last_name"""
 
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     username = db.Column(db.String(20), primary_key=True, nullable=False)
     password = db.Column(db.Text, nullable=False)
     email = db.Column(db.String(50), nullable=False)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
-    
+
+    @classmethod
+    def register(cls, username, pwd, email, first_name, last_name):
+
+        hashed = bcrypt.generate_password_hash(pwd)
+        hashed_utf8 = hashed.decode("utf8")
+
+        return cls(
+            username=username,
+            password=hashed_utf8,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+        )
+
+    @classmethod
+    def authenticate(cls, username, pwd):
+        """validate that a correct username and password is entered"""
+
+        u = User.query.filter_by(username=username).first()
+
+        if u and bcrypt.check_password_hash(u.password, pwd):
+            return u
+        else: 
+            return False
+
+
+# ####################################################################
+# feedback class
+
+class Feedback(db.Model):
+    """ feedback model with id, title, content, username """
+
+    __tablename__ = "feedback"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    username = db.Column(
+            db.String(20),
+            db.ForeignKey("users.username")
+    )
+
